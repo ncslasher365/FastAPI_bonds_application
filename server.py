d13dbf37fbeb88917790120249a9558a46969d29
sickboi <nikita.tsepochko20@gmail.com>
2023-07-06 14:43:59 +0300

    updated structure

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from fastapi.exceptions import ValidationError
 from fastapi.responses import JSONResponse
 
 from typing import List
-from app.models import Trade, UserResponse
+from models import Trade, UserResponse
 
 app = FastAPI(
     title="Trading application"
