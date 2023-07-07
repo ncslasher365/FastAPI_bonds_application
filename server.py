ca19df1da756a70b8e73515234397d18eee580ec
sickboi <nikita.tsepochko20@gmail.com>
2023-07-07 16:03:52 +0300

    server logic updated

--- server.py
+++ server.py
@@ -3,8 +3,12 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import ValidationError
 from fastapi.responses import JSONResponse
 
-from typing import List
-from models import Trade, UserResponse
+from fastapi_users import FastAPIUsers
+
+from auth.auth import auth_backend
+from auth.database import User
+from auth.manager import get_user_manager
+from auth.schemas import UserRead, UserCreate
 
 app = FastAPI(
     title="Trading application"
@@ -19,55 +23,19 @@ async def validation_exception_handler(request: Request, exc: ValidationError):
     )
 
 
-fake_users_db = [
-    {"id": 1, "role": "admin", "name": "Bob"},
-    {"id": 2, "role": "investor", "name": "John"},
-    {"id": 3, "role": "trader", "name": "Matt"},
-    {"id": 4, "role": "investor", "name": "Homer", "degree": [
-        {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"}
-    ]},
-]
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
 
-fake_trades_db = [
-    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 5.25},
-    {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 179, "amount": 8.66},
-]
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
 
-
-@app.get("/users/{user_id}", tags=["users"], response_model=UserResponse)
-async def parse_user_information(user_id: int):
-    try:
-        user_data = [user for user in fake_users_db if user.get("id") == user_id]
-    except Exception as e:
-        raise e
-    if user_data:
-        return {"status": 200, "data": user_data}
-    else:
-        return {"status": 404, "data": "User not found in the database!"}
-
-
-@app.post("/users/{user_id}", tags=["users"])
-async def update_user_name(user_id: int, new_name: str):
-    try:
-        current_user = list(filter(lambda user: user.get("id") == user_id, fake_users_db))[0]
-    except IndexError:
-        return {"status": 404, "data": "User data not found"}
-    if current_user:
-        current_user["name"] = new_name
-    return {"status": 201, "data": current_user}  # 201 - successfull post response
-
-
-@app.get("/trades", tags=["trades"])
-async def get_trades(limit: int = 1, offset: int = 0):
-    return fake_trades_db[offset: offset + limit]
-
-
-@app.post("/trades", tags=["trades"])
-async def add_trades(trades: List[Trade]):
-    fake_trades_db.extend(trades)
-    return {"status": 200, "data": fake_trades_db}
-
-
-@app.get("/trades/{trade_id}", tags=["trades"])
-async def get_trade_by_id(trade_id: int):
-    return [trade for trade in fake_trades_db if trade.get("id") == trade_id]
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
