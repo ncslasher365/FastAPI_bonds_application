6250e5ac0fd28dc24cf98cd94824c4af61851282
sickboi <nikita.tsepochko20@gmail.com>
2023-07-11 21:52:02 +0300

    added schema for operation creation

--- src/operations/schemas.py
+++ src/operations/schemas.py
@@ -1,15 +1,19 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class OperationBase(BaseModel):
     id: int
     quantity: str
-    figi: str
+    figi: str  # unique identifier
     instrument_type: str
-    date: datetime
+    date: datetime = Field(example='2023-07-09 12:48:56.289215')
     operation_type: str
 
     class Config:
         orm_mode = True
+
+
+class OperationCreate(OperationBase):
+    pass
