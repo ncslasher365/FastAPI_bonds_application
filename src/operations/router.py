2a98b5590ff014cf1eeade7e52330c8045f71425
sickboi <nikita.tsepochko20@gmail.com>
2023-07-11 21:52:14 +0300

    added new endpoint

--- src/operations/router.py
+++ src/operations/router.py
@@ -1,8 +1,8 @@
-from operations.schemas import OperationBase
+from operations.schemas import OperationBase, OperationCreate
 from typing import List
 
 from fastapi import APIRouter, Depends
-from sqlalchemy import select
+from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_async_session
@@ -22,3 +22,14 @@ async def get_specific_operations(
     query = select(Operation).where(Operation.operation_type == operation_type)
     query_result = await session.execute(query)
     return query_result.scalars().all()
+
+
+@router.post('/')
+async def create_specific_operation(
+        new_operation: OperationCreate,
+        session: AsyncSession = Depends(get_async_session)
+):
+    statement = insert(Operation).values(**new_operation.dict())
+    await session.execute(statement)
+    await session.commit()
+    return {"status": "success"}
